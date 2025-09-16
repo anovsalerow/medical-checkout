65a3333b63bbc8ec702f72fd820bdc0b497c37b1
a.novikov <anovikov@salerow.software>
2025-09-16 03:15:33 +0300

    [feature] create component "Footer", styles

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,58 @@
+import {Link} from 'react-router-dom';
+import { ABOUT_US, SHOPDOC, FAQ, BLOG, SOCIAL_LIST } from "../../constants";
+import { ListElement } from "../ListElement";
+import styles from "./_footer.module.scss";
+
+const getListOfLinks = (obj) => {
+    return Object.entries(obj).map(([text, path], index) => (
+        <ListElement styles={styles.item} key={index}>
+            <Link to={path}>{text}</Link>
+        </ListElement>
+    ));
+};
+
+const getListOfSocial = (arr) => {
+    return arr.map(({name, logo, path}, index) => (
+        <li key={index}>
+            <Link to={path}>
+                <img src={logo} alt={`${name} logo`}/>
+            </Link>
+        </li>
+    ));
+};
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.wrapper}>
+                <ul className={styles.list}>
+                    <li className={styles.title}><span>Shopdoc</span></li>
+                    {getListOfLinks(SHOPDOC)}
+                </ul>
+                <ul className={styles.list}>
+                    <ListElement styles={styles.title}><span>About Us</span></ListElement>
+                    {getListOfLinks(ABOUT_US)}
+                </ul>
+                <ul className={styles.list}>
+                    <li className={styles.title}><span>FAQ</span></li>
+                    {getListOfLinks(FAQ)}
+                </ul>
+                <ul className={styles.list}>
+                    <li className={styles.title}><span>BLOG</span></li>
+                    {getListOfLinks(BLOG)}
+                </ul>
+                <ul className={styles.list}>
+                    <li className={styles.title}><span>Contact us</span></li>
+                    <li className={styles.item}><span>Get in touch is easy</span></li>
+                    <li className={styles.item}><span>Follow us</span></li>
+                    <li className={styles.item}>
+                        <ul className={styles.social_list}>
+                            {getListOfSocial(SOCIAL_LIST)}
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <span className={styles.copyright}>Copyrights © 2021 Shopdoc. All Rights Reserved.</span>
+        </footer>
+    )
+}
