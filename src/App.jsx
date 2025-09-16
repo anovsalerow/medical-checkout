bac7f5e458259ed5a1758499a5b0712864e60798
a.novikov <anovikov@salerow.software>
2025-09-16 03:27:08 +0300

    [refactor] remane component "App.js" -> "App.jsx", add browser router, routes; remove unnecessary styles "App.css"

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import './App.css';
+import { LINKS } from './constants/constants';
+import { Footer } from './components/Footer';
+import { Main } from './components/Main';
+import { CheckoutPage } from './pages/CheckoupPage/CheckoutPage';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path={LINKS.homepage} element={<Main />}>Element Main
+            <Route path={LINKS.checkout} element={<CheckoutPage />}/>
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
