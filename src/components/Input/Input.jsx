88d541f70943df9778c5a9838d0184be7f68caed
a.novikov <anovikov@salerow.software>
2025-09-25 17:26:53 +0300

    [refactor] component Input: move 'res' spread to the end

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,11 @@
 import styles from './_input.module.scss';
 
 export const Input = ({type, name, placeholder, legend, ...rest}) => {
+    
     return (
         <fieldset className={styles.wrapper}>
             <legend>{legend}</legend>
-            <input {...rest} type={type} name={name} placeholder={placeholder} />
+            <input type={type} name={name} placeholder={placeholder} {...rest} />
         </fieldset>
     );
 }
