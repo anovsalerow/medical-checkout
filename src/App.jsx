6cc57757d4f4eda51613d43b5dd0d4c835913d67
a.novikov <anovikov@salerow.software>
2025-09-25 17:06:20 +0300

    [feature] app.jsx: import PrivateRoute, LoginPage, add route LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,31 @@ import { LINKS } from './constants/constants';
 import { Footer } from './components/Footer';
 import { Main } from './components/Main';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { LoginPage } from './pages/LoginPage';
 import { Header } from './components/Header';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "./store/slices/authSlice.js";
+import { PrivateRoute } from './components/PrivateRoute';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
   return (
     <Router>
       <div className="App">
         <Header />
         <Routes>
-          <Route path={LINKS.homepage} element={<Main />}>Element Main
-            <Route path={LINKS.checkout} element={<CheckoutPage />}/>
+          <Route path={LINKS.homepage} element={<Main />}>
+            <Route path={LINKS.checkout} element={
+              <PrivateRoute>
+                <CheckoutPage />
+              </PrivateRoute>}/>
+            <Route path={LINKS.login} element={<LoginPage />} />
           </Route>
         </Routes>
         <Footer />
