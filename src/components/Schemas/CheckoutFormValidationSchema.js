5adb40ad283c6f43dcf7c96ba4401389fa437bb3
a.novikov <anovikov@salerow.software>
2025-09-27 13:27:19 +0300

    [refactor] CheckoutFormValidationSchema: remove validation PWD, add validation paymentMethod and shippingMethod

--- src/components/Schemas/CheckoutFormValidationSchema.js
+++ src/components/Schemas/CheckoutFormValidationSchema.js
@@ -3,17 +3,28 @@ import Joi from 'joi';
 export const CheckoutFormValidationSchema = Joi.object({
     fullName: Joi.string().min(3).max(100).required().label('Full Name'),
     email: Joi.string().email({tlds: {allow: false}}).required().label('Email'),
-    password: Joi
-        .string()
-        .min(8)
-        .max(20)
-        .pattern(new RegExp(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/))
-        .required()
-        .label('Password'),
     phoneNumber: Joi.string().pattern(/^\+?[0-9\s\-()]{7,20}$/).required().label('Phone number'),
     address: Joi.string().min(5).max(200).required().label('Street Address'),
     city: Joi.string().min(2).max(100).required().label('City'),
     zipCode: Joi.string().pattern(/^[0-9]{4,10}$/).required().label('Zip Code'),
-    сountry: Joi.string().min(5).max(100).required().label('Country'),
-    promoCode: Joi.string().alphanum().min(5).max(20).optional().label('Promotion and Discount code')
+    country: Joi.string().min(5).max(100).required().label('Country'),
+    promoCode: Joi.string().alphanum().min(5).max(20).allow('').label('Promotion and Discount code'),
+    paymentMethod: Joi.string()
+    .valid('Credit Card', 'Paypal')
+    .required()
+    .label('Payment Method')
+    .messages({
+      'any.required': 'Please select a payment method',
+      'string.empty': 'Please select a payment method',
+      'any.only': 'Please select a valid payment method',
+    }),
+    shippingMethod: Joi.string()
+    .valid('Odeon Express', 'Gorgom Express', 'Cipay Jet', 'Eunioa Fast')
+    .required()
+    .label('Shipping Method')
+    .messages({
+      'any.required': 'Please select a shipping method',
+      'string.empty': 'Please select a shipping method',
+      'any.only': 'Please select a valid shipping method',
+    })
 });
