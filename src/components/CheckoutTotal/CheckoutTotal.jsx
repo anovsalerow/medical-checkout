bb508dd6ea4760678da9255314f457cdf3d5d7c1
a.novikov <anovikov@salerow.software>
2025-09-27 07:50:35 +0300

    [refactor] comsponent CheckoutTotal: add cart total price calculation

--- src/components/CheckoutTotal/CheckoutTotal.jsx
+++ src/components/CheckoutTotal/CheckoutTotal.jsx
@@ -1,14 +1,24 @@
 import styles from './_checkoutTotal.module.scss';
+import {CHECKOUT_TOTAL} from '../../constants/index';
 
 export const CheckoutTotal = ({data}) => {
+    const subtotal = data.reduce((sum, value) => sum + value.productId.price, 0);
+    const shippingPrice = CHECKOUT_TOTAL.shippingFee;
+    const total = subtotal ? subtotal + shippingPrice : 0;
     return (
         <ul className={styles.total__list}>
-            {data.map(({title, costs}, index) => (
-                <li className={styles.total__item} key={index}>
-                    <span className={styles.total__text}>{title}</span>
-                    <span className={styles.total__text}>{costs}</span>
-                </li>
-            ))}
+            <li className={styles.total__item}>
+                <span className={styles.total__text}>Sub total</span>
+                <span className={styles.total__text}>{`$${subtotal}`}</span>
+            </li>
+            <li className={styles.total__item}>
+                <span className={styles.total__text}>Shipping Fee</span>
+                <span className={styles.total__text}>{`$${shippingPrice}`}</span>
+            </li>
+            <li className={styles.total__item}>
+                <span className={styles.total__text}>Total</span>
+                <span className={styles.total__text}>{`$${total}`}</span>
+            </li>
         </ul>
     );
 }
