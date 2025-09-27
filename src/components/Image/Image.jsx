52e65b9e3c5d3fbf0e3d219ae837063570c997ef
a.novikov <anovikov@salerow.software>
2025-09-27 07:33:30 +0300

    [feractor] componennt Image: add '...rest'

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,5 +1,5 @@
-export const Image = ({src, altText, styles}) => {
+export const Image = ({src, altText, styles, ...rest}) => {
     return (
-        <img className={styles} src={src} alt={altText} />
+        <img className={styles} src={src} alt={altText} {...rest} />
     );
 }
