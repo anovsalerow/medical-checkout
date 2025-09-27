bce21732c443e397e1c5091b1425680d23120dca
a.novikov <anovikov@salerow.software>
2025-09-27 13:27:59 +0300

    [refactor] component InputRadio: add 'rest' param

--- src/components/InputRadio/InputRadio.jsx
+++ src/components/InputRadio/InputRadio.jsx
@@ -1,11 +1,11 @@
 import styles from './_inputRadio.module.scss';
 
-export const InputRadio = ({name, label, value}) => {
+export const InputRadio = ({name, label, value, ...rest}) => {
     return (
         <label htmlFor={label} className={styles.wrapper}>
             <span className={styles.rectangle} />
             <span className={styles.text}>{label}</span>
-            <input type='radio' id={label} name={name} value={value} />
+            <input type='radio' id={label} name={name} value={value} {...rest} />
         </label>
     );
 }
